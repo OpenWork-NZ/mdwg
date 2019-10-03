df232c5826afa568a160aabf1b2ab9f4408ff752
Adrian Cochrane <adrian@openwork.nz>
2019-10-04 12:01:00 +1300

    Configure PDF template.

--- pdf-converter.sh
+++ pdf-converter.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 cd _defs
-pandoc TableOfContents.md GuidanceIntro.md PatternGuide.md Tempate.md \
+pandoc TableOfContents.md GuidanceIntro.md PatternGuide.md Template.md \
     class-MD_Metadata.md MetadataIdentifier.md MetadataLinkage.md MetadataDate.md \
       MetadataContact.md MetadataLocale.md MetadataScope.md \
       MetadataLegalConstraints.md MetadataSecurityConstraints.md ParentMetadata.md \
@@ -20,5 +20,7 @@ ResourceEdition.md ResourceSeries.md ResourceResponsibleParty.md \
       class-MD_DataIdentification.md class-MD_Metadata.md class-MD_Identification.md \
       class-MD_Identifier.md class-MD_LegalConstraints.md class-MD_Scope.md \
       class-MD_SecurityConstraints.md PT_Locale.md \
-   -V mainfont="Open Sans" -V titlepage=true -V logo=
+   -V mainfont="Open Sans" -V titlepage=true -V titlepage-color=ddddff \
+     -V logo=../images/logo.png -V title="ICSM ISO19115-1 Metadata Good Practice Guide" \
+     -V header-right="3rd Oct 2019" -V footer-left="OpenWork" -V footer-center="Byron Cochrane" \
    --latex-engine=xelatex --template eisvogel.tex -o ../icsm.pdf
