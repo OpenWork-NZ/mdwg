fec11ed7ae75e955fc865087ddd7751a5f40b99f
Adrian Cochrane <adrian@openwork.nz>
2020-06-12 10:38:21 +1200

    Update the Metadata for Service Guide PDF.

--- docs/pdf-services-converter.sh
+++ docs/pdf-services-converter.sh
@@ -1,20 +1,22 @@
 #! /bin/sh
 cd _defs
-pandoc ContainsOperations.md CoupledResource.md CouplingType.md Parameter.md \
-        ServiceCitation.md ServiceIdentification.md ServiceProfile.md \
-        ServiceStandard.md ServiceType.md ServiceTypeVersion.md \
-    class-MD_Metadata.md MetadataIdentifier.md MetadataLinkage.md \
-        MetadataDate.md MetadataContact.md MetadataLocale.md \
-        MetadataLegalConstraints.md MetadataSecurityConstraints.md \
-        MetadataStandard.md ParentMetadata.md MetadataScope.md \
+pandoc MetadataForServicesGuide.md \
+    class-MD_Metadata.md MetadataIdentifier.md MetadataLinkage.md MetadataDate.md \
+      MetadataContact.md MetadataLocale.md MetadataLegalConstraints.md \
+      MetadataSecurityConstraints.md MetadataStandard.md MetadataScope.md \
     class-MD_Identification.md Abstract.md Purpose.md Status.md TopicCategory.md \
-            SpatialResolution.md ResourcePointOfContact.md \
-        AdditionalDocs.md SpatialRepresentationType.md \
-    ServiceIdentification.md ServiceTypeVersion.md CouplingType.md CoupledResource.md \
-        ServiceProfile.md ServiceStandard.md ContainsOperations.md Parameter.md \
+      SpatialResolution.md ResourcePointOfContact.md AdditionalDocs.md SpatialRepresentationType.md \
+    ResourceCitation.md ResourceTitle.md ResourceIdentifier.md ResourceDate.md \
+      ResourceEdition.md ResourceSeries.md ResourceResponsibleParty.md class-CI_OnlineResource.md \
+    ServiceIdentification.md ServiceType.md ServiceTypeVersion.md ServiceProfile.md \
+      ServiceStandard.md CouplingType.md CoupledResource.md ContainsOperations.md Parameter.md \
+    AssociatedResources.md ResourceFormat.md BrowseGraphic.md Keywords.md \
+      Maintenance.md class-MD_Constraints.md ResourceExtent.md GeographicExtent.md \
+        ExtentGeographicDescription.md ExtentBoundingBox.md VerticalExtent.md TemporalExtents.md \
+      SpatialReferenceSystem.md DistributionInfo.md \
    -V mainfont="Open Sans" -V titlepage=true -V titlepage-color=ddddff \
      -V logo=../images/logo.png -V title="ICSM ISO19115-1 Metadata Good Practice Guide" \
      -V header-center="Metadata Good Practice Guide" \
      -V header-right="3rd Oct 2019" -V footer-left="OpenWork" -V footer-center="Byron Cochrane" \
-   --latex-engine=xelatex --template ../eisvogel.tex -o ../icsm.pdf
+   --latex-engine=xelatex --template ../eisvogel.tex -o ../services.pdf
 
